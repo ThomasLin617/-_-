0771e9cb10f7222cecde2f38f5973a05671fc408
eric930617 <eric930617@gmail.com>
2024-06-09 00:53:15 +0800

    資料夾功能完善

--- 期末專題/server.py
+++ 期末專題/server.py
@@ -12,34 +12,45 @@ def create_folder_if_not_exists(folder):
         os.makedirs(folder)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/<path:subpath>')
+def index(subpath=''):
+    folder_path = os.path.join(app.config['UPLOAD_FOLDER'], subpath)
+    create_folder_if_not_exists(folder_path)
+    if os.path.isfile(folder_path):
+        return send_from_directory(app.config['UPLOAD_FOLDER'], subpath)
+    return render_template('index.html', subpath=subpath)
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
     folder = request.form.get('folder', '')
     create_folder_if_not_exists(os.path.join(app.config['UPLOAD_FOLDER'], folder))
     if 'file' not in request.files:
-        return redirect(url_for('index'))
+        return redirect(url_for('index', subpath=folder))
     file = request.files['file']
     if file.filename == '':
-        return redirect(url_for('index'))
+        return redirect(url_for('index', subpath=folder))
     if file:
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], folder, file.filename)
         file.save(filepath)
-        return redirect(url_for('index'))
+        return redirect(url_for('index', subpath=folder))
 
 @app.route('/download/<path:filename>')
 def download_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 @socketio.on('file_list')
-def handle_file_list():
+def handle_file_list(data):
+    subpath = data.get('subpath', '')
+    folder_path = os.path.join(app.config['UPLOAD_FOLDER'], subpath)
     files = []
-    for root, dirs, filenames in os.walk(app.config['UPLOAD_FOLDER']):
-        for filename in filenames:
-            files.append(os.path.relpath(os.path.join(root, filename), app.config['UPLOAD_FOLDER']))
-    emit('file_list', files)
+    dirs = []
+    for entry in os.listdir(folder_path):
+        full_path = os.path.join(folder_path, entry)
+        if os.path.isdir(full_path):
+            dirs.append(entry)
+        else:
+            files.append(entry)
+    emit('file_list', {'files': files, 'dirs': dirs, 'subpath': subpath})
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
